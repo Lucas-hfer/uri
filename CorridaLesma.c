c7c5b6cf02ba65f66ab64b47001f4ecf8524631e
Arthur <arthur.h.f13@gmail.com>
2020-12-07 23:10:20 -0300

    segunda tentiava corrida lesma

--- CorridaLesma.c
+++ CorridaLesma.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
  
 int main() {
- 
-    int i = 0, V[i], L, lv1 = 0, lv2 = 0, lv3 = 0;
-    scanf("%d", &L);
-    do{
+    int a,i,max;    
+    
+    while(scanf("%d",&a)!=EOF){
+        int V[a];
+        max = 0;
+
+        for(i=0; i < a; i++){
+            scanf("%d", &V[i]);
+            if(max < V[i]){
+                max = V[i];
+            }
+        }
+
+        if(max < 10){
+            printf("1\n");
+        } else if(max<20){
+            printf("2\n");
+        } else {
+            printf("3\n");
+        }
+    }
+    
+    /*do{
         scanf("%d\b", &V[i]);
         if(V[i] < 10){
             lv1 = 1;
@@ -17,6 +36,8 @@ int main() {
         }
         i++;
     }while(i < L);
+
+
     if ((lv3 > lv2) && (lv3 > lv1)){
         printf("%d", lv3);
     }
@@ -25,6 +46,6 @@ int main() {
     }
     if ((lv1 > lv3) && (lv1 > lv2)){
         printf("%d", lv1);
-    }
+    }*/
     return 0;
 }
